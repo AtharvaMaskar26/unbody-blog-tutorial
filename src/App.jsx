d8ddb10e8ef014fc5fcf604a0813625d9038569d
atharva26aug <atharvamaskar10@gmail.com>
2023-11-27 12:45:45 +0530

    Edited variables

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
     await fetch('https://graphql.unbody.io', {
       method: 'POST',
       headers: {
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6InMwR1JRSWdVNVhHUXVVU1IiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzAxNTk3Mzc3LCJpYXQiOjE3MDA5OTI1NzcsImlzcyI6Imh0dHBzOi8vbWRrcHFpeXBsa3B1dWhjdmxlYmUuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6ImRhOTQ3OWE1LWFiNTQtNGM2MS1hZjIzLTI2NGI3ODYxMjBkNCIsImVtYWlsIjoib3JrdXR3YXNteWlkZWFAZ21haWwuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJnb29nbGUiLCJwcm92aWRlcnMiOlsiZ29vZ2xlIl19LCJ1c2VyX21ldGFkYXRhIjp7ImF2YXRhcl91cmwiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NJYzdDRzRKOHZyemxZUHpEVXFBZl9UdC0xa1pucExmX3dMTVdyaHB5NHRDZz1zOTYtYyIsImVtYWlsIjoib3JrdXR3YXNteWlkZWFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZ1bGxfbmFtZSI6IkF0aGFydmEgTWFza2FyIiwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5nb29nbGUuY29tIiwibmFtZSI6IkF0aGFydmEgTWFza2FyIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0ljN0NHNEo4dnJ6bFlQekRVcUFmX1R0LTFrWm5wTGZfd0xNV3JocHk0dENnPXM5Ni1jIiwicHJvdmlkZXJfaWQiOiIxMDA1NzI0MjU2OTcxMzA4MDkyNjQiLCJzdWIiOiIxMDA1NzI0MjU2OTcxMzA4MDkyNjQifSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJvYXV0aCIsInRpbWVzdGFtcCI6MTY5OTg1NDMxNX1dLCJzZXNzaW9uX2lkIjoiZmVlZDJkODMtYWEyYi00MzU5LWJlOWEtYjk1ZGY0ZTlkOGYyIn0.mECXzHCxyi0ZOpPWUHsJC9IHV6uVR5-r0lwtgrcwc_0',
-        'X-Project-Id': '0093ce10-544e-41cf-bf88-3e607f2a2c86',
+        'Authorization': `Bearer ${import.meta.env.VITE_TOKEN}`,
+        'X-Project-Id': `${import.meta.env.VITE_PROJECT_ID}`,
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
